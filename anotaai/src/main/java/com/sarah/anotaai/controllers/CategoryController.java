2171ab7b0fc27789681c84bb3832fbe53c5df09b
Sarah Enaile <sarahenaile@gmail.com>
2025-01-17 19:47:34 -0300

    - Tratando erro em não deixar o cliente inserir um novo produto sem alterar Category id ou ownerId

--- anotaai/src/main/java/com/sarah/anotaai/controllers/CategoryController.java
+++ anotaai/src/main/java/com/sarah/anotaai/controllers/CategoryController.java
@@ -3,7 +3,6 @@ package com.sarah.anotaai.controllers;
 import com.sarah.anotaai.domain.category.Category;
 import com.sarah.anotaai.domain.category.CategoryDTO;
 import com.sarah.anotaai.services.CategoryService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +12,32 @@ import java.util.List;
 @RequestMapping("/api/category")
 public class CategoryController {
 
-    private CategoryService service;
+    private final CategoryService service;
 
-    public CategoryController(CategoryService service){
+    public CategoryController(CategoryService service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity<Category> create(@RequestBody CategoryDTO categoryData){
+    public ResponseEntity<Category> create(@RequestBody CategoryDTO categoryData) {
         Category newCategory = this.service.create(categoryData);
         return ResponseEntity.ok().body(newCategory);
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> getAll(){
+    public ResponseEntity<List<Category>> getAll() {
         List<Category> categories = this.service.getAll();
         return ResponseEntity.ok().body(categories);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> update(@PathParam("id") String id,@RequestBody CategoryDTO categoryData){
-        Category updateCategory = this.service.update(id, categoryData);
-        return ResponseEntity.ok().body(updateCategory);
+    public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryData) {
+        Category updatedCategory = this.service.update(id, categoryData);
+        return ResponseEntity.ok().body(updatedCategory);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Category> delete(@PathParam("id") String id){
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
         this.service.delete(id);
         return ResponseEntity.noContent().build();
     }
--- anotaai/src/main/java/com/sarah/anotaai/controllers/ProductController.java
+++ anotaai/src/main/java/com/sarah/anotaai/controllers/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping(" ")
 public class ProductController {
     private ProductService service;
 
@@ -30,12 +30,12 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@PathParam("id") String id, @RequestBody ProductDTO productData){
+    public ResponseEntity<Product> update(@PathVariable("id") String id, @RequestBody ProductDTO productData){
         Product updateProduct = this.service.update(id, productData);
         return ResponseEntity.ok().body(updateProduct);
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<Product> delete(@PathParam("id") String id){
+    public ResponseEntity<Product> delete(@PathVariable("id") String id){
         this.service.delete(id);
         return ResponseEntity.noContent().build();
     }
